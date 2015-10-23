5b5173cc2aea07fd6091c91b770768a79dd904ff
Niklas Närhinen <niklas@narhinen.net>
2015-10-23 08:20:55 +0300

    Initial version

---
+++ flowplayer-thumbnails.js
@@ -0,0 +1,57 @@
+/* jshint undef:true, browser:true, node:true */
+
+(function(flowplayer) {
+  "use strict";
+  flowplayer(function(api, root) {
+    var common = flowplayer.common,
+        bean = flowplayer.bean,
+        timeline = common.find('.fp-timeline', root)[0],
+        timelineTooltip = common.find('.fp-timeline-tooltip', root)[0];
+
+    api.on('ready', function(ev, a, video) {
+      cleanup();
+      var c = video.thumbnails || api.conf.thumbnails;
+      if (!c) return;
+      if (c.preload !== false) {
+        preloadImages(c.template, video.duration);
+      }
+      bean.on(root, 'mousemove.thumbnails', '.fp-timeline', function(ev) {
+        var x = ev.pageX || ev.clientX,
+            delta = x - common.offset(timeline).left,
+            percentage = delta / common.width(timeline),
+            seconds = Math.round(percentage * api.video.duration);
+        if (seconds < 1) return;
+        var width = c.width || 200;
+        common.css(timelineTooltip, {
+          width: width + 'px',
+          'padding-bottom': (api.conf.ratio * width) + 'px',
+          'background-image': "url('" + c.template.replace('{time}', seconds) + "')",
+          'background-repeat': 'no-repeat',
+          'background-size': 'cover',
+          'background-position': 'center'
+        });
+      });
+    });
+    function cleanup() {
+      bean.off(root, '.thumbnails');
+      common.css(timelineTooltip, {
+        width: '',
+        'padding-bottom': '',
+        'background-image': '',
+        'background-repeat': '',
+        'background-size': '',
+        'background-position': ''
+      });
+    }
+
+  });
+
+  function preloadImages(tmpl, max, start) {
+    if (typeof start === 'undefined') start = 1;
+    while (start <= max) {
+      var img = new Image();
+      img.src = tmpl.replace('{time}', start);
+      start++;
+    }
+  }
+}(typeof module === "object" && module.exports ? require('flowplayer') : window.flowplayer));
