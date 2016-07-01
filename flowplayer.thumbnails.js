2b7ec1d14ba5b77b54dfc76473cf33661c41d796
Christian Ebert <blacktrash@gmx.net>
2016-07-01 09:50:45 +0100

    Introduce lazy loading of thumbs (#11)
    Avoids 'black' thumbs at the minor price that the 'previous' thumb is
    shown while the correct one is loaded. UI-wise this minor distraction
    feels surprisingly organic compared to a 'black void'.
    
    This demand loading is the new default behaviour.
    
    Thanks to @VarunAgw for the proposal and implementation tips.

--- flowplayer.thumbnails.js
+++ flowplayer.thumbnails.js
@@ -54,6 +54,9 @@
                 time_format = c.time_format || function (t) {
                     return t;
                 },
+                thumb = c.lazyload !== false
+                    ? new Image()
+                    : null,
                 ratio = video.height / video.width,
                 preloadImages = function (tmpl, max, start) {
                     max = Math.floor(max / interval);
@@ -82,7 +85,20 @@
                 var x = ev.pageX || ev.clientX,
                     delta = x - common.offset(timeline).left,
                     percentage = delta / common.width(timeline),
-                    seconds = Math.round(percentage * api.video.duration);
+                    seconds = Math.round(percentage * api.video.duration),
+                    url,
+                    displayThumb = function () {
+                        common.css(timelineTooltip, {
+                            width: (height / ratio) + 'px',
+                            height: height + 'px',
+                            'background-image': "url('" + url + "')",
+                            'background-repeat': 'no-repeat',
+                            'background-size': 'cover',
+                            'background-position': 'center',
+                            'border': '1px solid #333',
+                            'text-shadow': '1px 1px #000'
+                        });
+                    };
 
                 // 2nd condition safeguards at out of range retrieval attempts
                 if (seconds < 0 || seconds > Math.round(api.video.duration)) {
@@ -91,17 +107,15 @@
                 // enables greater interval than one second between thumbnails
                 seconds = Math.floor(seconds / interval);
 
-                common.css(timelineTooltip, {
-                    width: (height / ratio) + 'px',
-                    height: height + 'px',
-                    // {time} template expected to start at 1, video time/first frame starts at 0
-                    'background-image': "url('" + template.replace('{time}', time_format(seconds + 1)) + "')",
-                    'background-repeat': 'no-repeat',
-                    'background-size': 'cover',
-                    'background-position': 'center',
-                    'border': '1px solid #333',
-                    'text-shadow': '1px 1px #000'
-                });
+                // {time} template expected to start at 1, video time/first frame starts at 0
+                url = template.replace('{time}', time_format(seconds + 1));
+
+                if (c.lazyload !== false) {
+                    thumb.src = url;
+                    bean.on(thumb, 'load', displayThumb);
+                } else {
+                    displayThumb();
+                }
             });
         });
 
